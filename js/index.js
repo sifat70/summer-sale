5051333cac60ae8a4ca4b3161b9fac21d9dd2afb
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-08-20 07:57:57 +0600

    discound added

--- js/index.js
+++ js/index.js
@@ -8,8 +8,23 @@ function handleCLikBtn(target, price) {
     selectedItemContainer.appendChild(li);
 
     const TotalPrice = price;
-    total = parseInt(total) + parseInt(price);
+    total = parseFloat(total) + parseFloat(price);
     document.getElementById("total-price").innerText = total;
-
-    
+    return total;
 }
+
+const discoundTotal = document.getElementById('discound-btn').addEventListener('click', function(){
+    const discoundCalculation = parseFloat(total * 0.80);
+    const discoundTaka = parseFloat((total - discoundCalculation).toFixed(2));
+    
+
+
+    const discoundPrice = document.getElementById('discound-price');
+    discoundPrice.innerText = parseFloat(discoundTaka) ;
+
+    const totalPayAmound = document.getElementById('total-pay-amount');
+
+    // const finalAmound = parseFloat(total) - parseFloat(discoundPrice);
+    
+    // totalPayAmound.innerText = finalAmound;
+})
